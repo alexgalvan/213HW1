0f7f8d4576659ee2eb70719c5dcda89010269749
alexgalvan <alexgalvan0630@yahoo.com>
2021-02-08 18:39:17 -0500

    Update RunProject1.java

--- src/RunProject1.java
+++ src/RunProject1.java
@@ -2,7 +2,7 @@ package src;
 
 /**
  * the entry of the program
- * @author  Joel Wallace, Yuan Zhao
+ * @author  Alexander Galvan, Yuan Zhao
  */
 public class RunProject1 {
 	public static void main(String[] args) {
